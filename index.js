885d5fab83b326a385da47a4433d6099edc1cd0e
Max Vovk <maksemvovk@gmail.com>
2019-10-07 23:31:03 +0300

    rm trash code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dictionary = require('./dictionary')
 function transliterationSorting (list, field, languages = 'en_ru') {
   const vocabruary = getDictionary(languages)
   const tr = translit(vocabruary)
-  console.log(list.map(it => ({ ...it, translit: tr(it[field]) })).sort(compare))
+
   return list.map(it => ({ ...it, translit: tr(it[field]) })).sort(compare)
 }
 
